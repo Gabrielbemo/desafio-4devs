5ba978f4673251bf5934b8c44bf16939f5ffedef
Gabrielbemo <gabrielbemoura@gmail.com>
2018-09-26 13:50:21 -0300

    Navbar criada

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-var server = app.listen(8080, function () {
+var server = app.listen(1750, function () {
     console.log('Servidor ON');
 })
